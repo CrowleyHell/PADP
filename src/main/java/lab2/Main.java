8f97e979a22b958810d76501ab27b539b4a9ecbb
CrowleyHell <n.evvio@bk.ru>
2020-10-16 15:32:28 +0300

    Scripted auto-commit on change (2020-10-16 15:32:28) by gitwatch.sh

--- src/main/java/lab2/Main.java
+++ src/main/java/lab2/Main.java
@@ -5,6 +5,8 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class Main {
@@ -15,13 +17,19 @@ public class Main {
         }
         Job job = Job.getInstance();
         job.setJarByClass(Main.class);
-        job.setJobName("Word count");
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        job.setMapperClass(WordMapper.class);
-        job.setReducerClass(WordReducer.class);
+        job.setJobName("Lab2");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlyJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirJoinMapper.class);
+
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass(TextPart.class);
+        job.setGroupingComparatorClass(TextComp.class);
+        job.setReducerClass(ReducerJ.class);
+        job.setMapOutputKeyClass(DoubleKey.class);
+        job.setMapOutputValueClass(Text.class);
+
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setOutputValueClass(Text.class);
         job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
