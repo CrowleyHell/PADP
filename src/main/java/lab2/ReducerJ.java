8f2ab7deb434ca01eba163240f2730592ca2a971
CrowleyHell <n.evvio@bk.ru>
2020-10-21 18:15:29 +0300

    Scripted auto-commit on change (2020-10-21 18:15:29) by gitwatch.sh

--- src/main/java/lab2/ReducerJ.java
+++ src/main/java/lab2/ReducerJ.java
@@ -5,6 +5,8 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+
 public class ReducerJ extends Reducer<Text, IntWritable, Text, LongWritable> {
-    
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
 }
