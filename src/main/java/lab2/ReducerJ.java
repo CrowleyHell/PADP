31e0484b7eb7a9f3bac39e06cfab0416f59e3212
CrowleyHell <n.evvio@bk.ru>
2020-10-21 18:25:49 +0300

    Scripted auto-commit on change (2020-10-21 18:25:49) by gitwatch.sh

--- src/main/java/lab2/ReducerJ.java
+++ src/main/java/lab2/ReducerJ.java
@@ -9,5 +9,7 @@ import java.util.Iterator;
 public class ReducerJ extends Reducer<WritableComp, Text, Text, Text> {
     protected void reduce(WritableComp key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
+        Text nameAir = iter.next();
+        
     }
 }
