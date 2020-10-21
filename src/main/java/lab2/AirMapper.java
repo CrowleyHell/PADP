345f734e9637c4e35ab51ad0a9e790a762f392dd
CrowleyHell <n.evvio@bk.ru>
2020-10-21 17:28:40 +0300

    Scripted auto-commit on change (2020-10-21 17:28:40) by gitwatch.sh

--- src/main/java/lab2/AirMapper.java
+++ src/main/java/lab2/AirMapper.java
@@ -4,10 +4,10 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
+
 
 import java.io.IOException;
 
-public class AirMapper extends Reducer<Text, IntWritable, Text, LongWritable> {
-    public void map (LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException
+public class AirMapper extends Mapper<Text, IntWritable, Text, LongWritable> {
+    public void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException
 }
