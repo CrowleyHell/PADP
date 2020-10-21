2eb61626028620dc3878f3619b8f236d43ffaccb
CrowleyHell <n.evvio@bk.ru>
2020-10-21 18:06:57 +0300

    Scripted auto-commit on change (2020-10-21 18:06:57) by gitwatch.sh

--- src/main/java/lab2/GroupComp.java
+++ src/main/java/lab2/GroupComp.java
@@ -1,7 +1,8 @@
 package lab2;
 
 import org.apache.hadoop.io.RawComparator;
+import org.apache.hadoop.io.WritableComparable;
 
 public class GroupComp implements RawComparator {
-    public int compare ()
+    public int compare (WritableComparable)
 }
