339e02df3e655e899193d0f0af39c9aeea515332
CrowleyHell <n.evvio@bk.ru>
2020-10-21 16:58:36 +0300

    Scripted auto-commit on change (2020-10-21 16:58:36) by gitwatch.sh

--- src/main/java/lab2/WritableComp.java
+++ src/main/java/lab2/WritableComp.java
@@ -2,6 +2,6 @@ package lab2;
 
 import org.apache.hadoop.io.WritableComparable;
 
-public class WritableComp implements WritableComparable {
-
+public class WritableComp implements WritableComparable<WritableComp> {
+    
 }
