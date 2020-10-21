a9cc1e101da9214717531103edf6bccdb19137b2
CrowleyHell <n.evvio@bk.ru>
2020-10-21 17:06:51 +0300

    Scripted auto-commit on change (2020-10-21 17:06:51) by gitwatch.sh

--- src/main/java/lab2/WritableComp.java
+++ src/main/java/lab2/WritableComp.java
@@ -6,7 +6,11 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public class WritableComp implements WritableComparable<WritableComp> {
+    private int codeAir, flagAir;
     public void write(DataOutput out) throws IOException {
-        
+        out.writeInt(codeAir);
+        out.writeInt(flagAir);
     }
+
+    public void readFile()
 }
