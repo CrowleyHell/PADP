185c0e10ee21e1dcb59dcdbf0b0ce661c1027776
CrowleyHell <n.evvio@bk.ru>
2020-10-22 20:58:55 +0300

    Scripted auto-commit on change (2020-10-22 20:58:55) by gitwatch.sh

--- src/main/java/lab2/ReducerJ.java
+++ src/main/java/lab2/ReducerJ.java
@@ -10,8 +10,8 @@ public class ReducerJ extends Reducer<WritableComp, Text, Text, Text> {
     protected void reduce(WritableComp key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
         Text nameAir = new Text(iter.next());
-        float delTimePrev = Float.parseFloat(iter.next().toString());
         if (iter.hasNext()) {
+            float delTimePrev = Float.parseFloat(iter.next().toString());
             float minDel = delTimePrev;
             float maxDel = delTimePrev;
             float sum = delTimePrev;
