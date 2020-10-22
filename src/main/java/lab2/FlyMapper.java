60271a7c65bae3582309f3e19cdd3e440b4cf42e
CrowleyHell <n.evvio@bk.ru>
2020-10-22 20:32:35 +0300

    Scripted auto-commit on change (2020-10-22 20:32:35) by gitwatch.sh

--- src/main/java/lab2/FlyMapper.java
+++ src/main/java/lab2/FlyMapper.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class FlyMapper extends Mapper<LongWritable, Text, WritableComp, Text> {
     public void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
         String[] str = value.toString().split(",");
-        if (Float.parseFloat(str[18]) != 0 && !str[18].isEmpty()){
+        if (Float.parseFloat(str[18]) != 0 && !str[18].isEmpty() && key != new LongWritable(0)){
             context.write(new WritableComp(Integer.parseInt(str[14]), 1), new Text(str[18]));
-        }   
+        }
     }
 }
 
