e8b87a2ccbff85fcc12f90b10b5cec292602c433
CrowleyHell <n.evvio@bk.ru>
2020-10-30 19:11:48 +0300

    Scripted auto-commit on change (2020-10-30 19:11:48) by gitwatch.sh

--- src/main/java/lab2/AirMapper.java
+++ src/main/java/lab2/AirMapper.java
@@ -8,11 +8,14 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class AirMapper extends Mapper<LongWritable, Text, WritableComp, Text> {
+    protected static final String REGcomma = "[,]";
+    protected static final String REGamper = "[&]";
+    protected static final String REGslash = "[\"]";
     public void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
         String[] str = value.toString()
-                .replaceFirst(",", "&")
-                .replaceAll("\"", "")
-                .split("&");
+                .replaceFirst(REGcomma, REGamper)
+                .replaceAll(REGslash, "")
+                .split(REGamper);
         if (key.get() != 0){
             context.write(new WritableComp(Integer.parseInt(str[0]), 0), new Text(str[1]));
         }
