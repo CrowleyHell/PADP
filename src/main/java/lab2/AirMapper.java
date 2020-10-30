0f50887692adcb762839306795734b8f19485895
CrowleyHell <n.evvio@bk.ru>
2020-10-30 19:16:59 +0300

    Scripted auto-commit on change (2020-10-30 19:16:59) by gitwatch.sh

--- src/main/java/lab2/AirMapper.java
+++ src/main/java/lab2/AirMapper.java
@@ -8,14 +8,14 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class AirMapper extends Mapper<LongWritable, Text, WritableComp, Text> {
-    protected static final String REGcomma = "[,]";
-    protected static final String REGamper = "[&]";
-    protected static final String REGslash = "[\"]";
+    private static final String REGCOMMA = "[,]";
+    private static final String REGAMPER = "[&]";
+    private static final String REGSLASH = "[\"]";
     public void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
         String[] str = value.toString()
-                .replaceFirst(REGcomma, REGamper)
-                .replaceAll(REGslash, "")
-                .split(REGamper);
+                .replaceFirst(REGCOMMA, REGAMPER)
+                .replaceAll(REGSLASH, "")
+                .split(REGAMPER);
         if (key.get() != 0){
             context.write(new WritableComp(Integer.parseInt(str[0]), 0), new Text(str[1]));
         }
--- src/main/java/lab2/FlyMapper.java
+++ src/main/java/lab2/FlyMapper.java
@@ -8,12 +8,12 @@ import java.io.IOException;
 
 
 public class FlyMapper extends Mapper<LongWritable, Text, WritableComp, Text> {
-    protected static final int destAirID = 14;
-    protected static final int destAirID = 14;
+    private static final int destAirID = 14;
+    private static final int delay = 18;
     public void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
         String[] str = value.toString().split(",");
-        if (key.get() != 0 && !str[18].isEmpty() && Float.parseFloat(str[18]) != 0){
-            context.write(new WritableComp(Integer.parseInt(str[14]), 1), new Text(str[18]));
+        if (key.get() != 0 && !str[delay].isEmpty() && Float.parseFloat(str[delay]) != 0){
+            context.write(new WritableComp(Integer.parseInt(str[destAirID]), 1), new Text(str[delay]));
         }
     }
 }
