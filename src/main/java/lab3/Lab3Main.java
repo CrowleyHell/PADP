51e6fd7a4364a1acfb8a105aee2e06b596ca0874
CrowleyHell <n.evvio@bk.ru>
2020-11-18 17:01:20 +0300

    Scripted auto-commit on change (2020-11-18 17:01:20) by gitwatch.sh

--- src/main/java/lab3/Lab3Main.java
+++ src/main/java/lab3/Lab3Main.java
@@ -1,4 +1,10 @@
 package lab3;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class Lab3Main {
+    SparkConf conf = new SparkConf().setAppName("lab4");
+    JavaSparkContext sc = new JavaSparkContext(conf);
+    
 }
