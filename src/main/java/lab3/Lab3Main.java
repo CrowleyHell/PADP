e8d69cfb2f89f883f33b7e1cb0dc943b94c6e4fc
CrowleyHell <n.evvio@bk.ru>
2020-11-18 18:49:03 +0300

    Scripted auto-commit on change (2020-11-18 18:49:03) by gitwatch.sh

--- src/main/java/lab3/Lab3Main.java
+++ src/main/java/lab3/Lab3Main.java
@@ -25,6 +25,6 @@ public class Lab3Main {
             .groupByKey()
             .mapValues(s -> new FlightStats(s.iterator()));
     Map<Long, String> dictionaryMap = dictionaryAir.collectAsMap();
-    final Broadcast<Map<Long, String>> dictionaryBroadcasted = dictionaryMap.
+    final Broadcast<Map<Long, String>> dictionaryBroadcasted = dictionaryMap.b
 
 }
