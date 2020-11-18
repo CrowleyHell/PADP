67a8a9c948c4b9d6208b7e57d71829e5700e0621
CrowleyHell <n.evvio@bk.ru>
2020-11-18 17:10:02 +0300

    Scripted auto-commit on change (2020-11-18 17:10:02) by gitwatch.sh

--- src/main/java/lab3/Lab3Main.java
+++ src/main/java/lab3/Lab3Main.java
@@ -10,7 +10,7 @@ public class Lab3Main {
     JavaSparkContext sc = new JavaSparkContext(conf);
     JavaRDD<String> airData = sc.textFile("airData.csv");
     JavaRDD<String> flyData = sc.textFile("flyData.csv");
-    JavaPairRDD<Long, String> dictionaryAir = airData.mapToPair()
+    JavaPairRDD<Long, String> dictionaryAir = airData.mapToPair(s -> )
 
 
 }
