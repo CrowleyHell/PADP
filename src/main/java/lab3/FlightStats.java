f19f278b6ecbc330d58d4537c5834151c88c5715
CrowleyHell <n.evvio@bk.ru>
2020-11-18 18:21:12 +0300

    Scripted auto-commit on change (2020-11-18 18:21:12) by gitwatch.sh

--- src/main/java/lab3/FlightStats.java
+++ src/main/java/lab3/FlightStats.java
@@ -3,6 +3,24 @@ package lab3;
 import java.io.Serializable;
 
 public class FlightStats implements Serializable {
-    int cancelledFlights, delayedFlights, ;
+    int cancelledFlights, delayedFlights, maxDelayedFlight;
+
+    public FlightStats(){
+        this.cancelledFlights = 0;
+        this.delayedFlights = 0;
+        this.maxDelayedFlight = 0;
+    }
+
+    public int add(String delay){
+        if (delay.isEmpty()){
+            cancelledFlights++;
+        } else {
+            delayedFlights++;
+            if (Integer.parseInt(delay) > maxDelayedFlight){
+                maxDelayedFlight = delayedFlights;
+            }
+        }
+    }
+
 
 }
