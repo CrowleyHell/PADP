d7f5c0559534c2db2b180ea96de040d6785132d8
CrowleyHell <n.evvio@bk.ru>
2020-11-18 19:06:46 +0300

    Scripted auto-commit on change (2020-11-18 19:06:46) by gitwatch.sh

--- src/main/java/lab3/FlightStats.java
+++ src/main/java/lab3/FlightStats.java
@@ -4,15 +4,18 @@ import java.io.Serializable;
 import java.util.Iterator;
 
 public class FlightStats implements Serializable {
-    private int cancelledFlights, delayedFlights;
-    private float maxDelayedFlight;
+    private int originID, arrivalID;
+    private float maxDelayedFlight, perDelay, perCancelled;
+    private String originPortName, arrivalPortName;
 
-    public FlightStats(Iterator<String> delays){
-        this.cancelledFlights = 0;
-        this.delayedFlights = 0;
-        this.maxDelayedFlight = 0;
+    public static FlightStats CountStats(Iterator<String> delays){
+        int cancelledFlights = 0;
+        int delayedFlights = 0;
+        int generalCountFlights = 0;
+        float maxDelayedFlight = 0;
         while (delays.hasNext()){
             String delay = delays.next();
+            generalCountFlights++;
             if (delay.isEmpty()){
                 cancelledFlights++;
             } else if (!delay.equals("0.00")){
@@ -22,6 +25,19 @@ public class FlightStats implements Serializable {
                 }
             }
         }
+        float perDelay = delayedFlights*100/generalCountFlights;
+        float perCancelled = cancelledFlights*100/generalCountFlights;
+        return new FlightStats(maxDelayedFlight, perDelay, perCancelled);
+    }
+
+    public FlightStats(float maxDelayedFlight, float perDelay, float perCancelled) {
+        this.maxDelayedFlight = maxDelayedFlight;
+        this.perDelay = perDelay;
+        this.perCancelled = perCancelled;
+    }
+
+    public String formFinalStats(){
+
     }
 
     @Override
