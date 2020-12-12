bcb619e7e56b328b43f7957c4b2a7a60d4f2e260
CrowleyHell <n.evvio@bk.ru>
2020-12-12 14:25:55 +0300

    Scripted auto-commit on change (2020-12-12 14:25:55) by gitwatch.sh

--- src/main/java/lab4/ActorSave.java
+++ src/main/java/lab4/ActorSave.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ActorSave extends AbstractActor {
-    private Map<Integer, String> store = new HashMap<>();
+    private Map<Integer, Map<String, String>> store = new HashMap<>();
     @Override
     public Receive createReceive(){
         return ReceiveBuilder.create().match(TestResult.class, m -> {
