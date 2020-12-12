6169fd32a4be7326b216aac5a76accd64dbdbbdc
CrowleyHell <n.evvio@bk.ru>
2020-12-12 15:13:07 +0300

    Scripted auto-commit on change (2020-12-12 15:13:07) by gitwatch.sh

--- src/main/java/lab4/ExecutorActor.java
+++ src/main/java/lab4/ExecutorActor.java
@@ -17,7 +17,7 @@ public class ExecutorActor extends AbstractActor {
             Object result = invocable.invokeFunction(m.getFunctionName(), m.getParams().toArray().toString());
             String response;
             if (result.equals(m.getExpectedResult())) {
-                sender().tell(new Result(m.getPackageID(), m.getTestName()), );
+                sender().tell(new Result(m.getPackageID(), m.getTestName()), true );
             } else {
                 response = "false";
             }
