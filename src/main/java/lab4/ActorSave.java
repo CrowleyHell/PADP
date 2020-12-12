de243a7a987d6d6898c5a10e6c1a55d2a11c34b5
CrowleyHell <n.evvio@bk.ru>
2020-12-12 13:55:48 +0300

    Scripted auto-commit on change (2020-12-12 13:55:48) by gitwatch.sh

--- src/main/java/lab4/ActorSave.java
+++ src/main/java/lab4/ActorSave.java
@@ -3,8 +3,9 @@ package lab4;
 
 import akka.actor.AbstractActor;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ActorSave extends AbstractActor {
-    private Map<String, String> store = 
+    private Map<String, String> store = new HashMap<>()
 }
