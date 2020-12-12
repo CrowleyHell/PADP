896f732eeee5c31f87405f8e517aaa7534fc2d7c
CrowleyHell <n.evvio@bk.ru>
2020-12-12 14:22:31 +0300

    Scripted auto-commit on change (2020-12-12 14:22:31) by gitwatch.sh

--- src/main/java/lab4/TestResult.java
+++ src/main/java/lab4/TestResult.java
@@ -2,18 +2,35 @@ package lab4;
 
 public class TestResult {
     private int ID;
-    private String name;
+    private String name, res;
 
-    public TestResult(int ID, String name) {
+    public TestResult(int ID, String name, String res) {
         this.ID = ID;
         this.name = name;
+        this.res = res;
     }
 
     public int getID() {
         return ID;
     }
 
+    public String getRes() {
+        return res;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRes(String res) {
+        this.res = res;
     }
 }
