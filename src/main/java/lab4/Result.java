64af780bc11ce441083e785cfd80c30b80ba729b
CrowleyHell <n.evvio@bk.ru>
2020-12-12 14:36:54 +0300

    Scripted auto-commit on change (2020-12-12 14:36:54) by gitwatch.sh

--- src/main/java/lab4/Result.java
+++ src/main/java/lab4/Result.java
@@ -5,5 +5,25 @@ import java.util.Map;
 public class Result {
     private int id;
     private Map<Integer, String> result;
-    
+
+    public Result(int id, Map<Integer, String> result) {
+        this.id = id;
+        this.result = result;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Map<Integer, String> getResult() {
+        return result;
+    }
+
+    public void setResult(Map<Integer, String> result) {
+        this.result = result;
+    }
 }
