199c36f1453958b13cf91929e031e9bf60d21864
CrowleyHell <n.evvio@bk.ru>
2020-12-12 14:20:28 +0300

    Scripted auto-commit on change (2020-12-12 14:20:28) by gitwatch.sh

--- src/main/java/lab4/TestResult.java
+++ src/main/java/lab4/TestResult.java
@@ -4,5 +4,16 @@ public class TestResult {
     private int ID;
     private String name;
 
-    
+    public TestResult(int ID, String name) {
+        this.ID = ID;
+        this.name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
