35f4d7071c1cefabe50eb93a6d8ad49049d50f73
CrowleyHell <n.evvio@bk.ru>
2020-12-12 15:57:15 +0300

    Scripted auto-commit on change (2020-12-12 15:57:15) by gitwatch.sh

--- src/main/java/lab4/Router.java
+++ src/main/java/lab4/Router.java
@@ -6,7 +6,7 @@ import akka.http.javadsl.server.Route;
 
 public class Router {
     private Route RouteCreate(ActorSystem actorSystem, ActorRef actorRef){
-        return bind(get)
+        return bind(get(()))
     }
 
 }
