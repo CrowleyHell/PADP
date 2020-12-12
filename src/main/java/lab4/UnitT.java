de257036e6ffcf388257faecbb720a7e073df129
CrowleyHell <n.evvio@bk.ru>
2020-12-12 14:52:26 +0300

    Scripted auto-commit on change (2020-12-12 14:52:26) by gitwatch.sh

--- src/main/java/lab4/UnitT.java
+++ src/main/java/lab4/UnitT.java
@@ -3,6 +3,15 @@ package lab4;
 import java.util.ArrayList;
 
 public class UnitT {
-    private String packageID, jsScript, functionName, testName, expectedResulr;
-    private ArrayList<Integer>
+    private final String packageID, jsScript, functionName, testName, expectedResulr;
+    private final ArrayList<Integer> params;
+
+    public UnitT(String packageID, String jsScript, String functionName, String testName, String expectedResulr, ArrayList<Integer> params) {
+        this.packageID = packageID;
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.testName = testName;
+        this.expectedResulr = expectedResulr;
+        this.params = params;
+    }
 }
