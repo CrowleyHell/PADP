12800f7bee56483d172a0ce23549760c22ef1c2f
CrowleyHell <n.evvio@bk.ru>
2020-12-12 14:46:38 +0300

    Scripted auto-commit on change (2020-12-12 14:46:38) by gitwatch.sh

--- src/main/java/lab4/ExecutorActor.java
+++ src/main/java/lab4/ExecutorActor.java
@@ -6,7 +6,7 @@ import akka.japi.pf.ReceiveBuilder;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
-public class ExecutorActor {
+public class ExecutorActor extends {
     return ReceiveBuilder().match
     ScriptEngine engine = new ScriptEngineManager().getEngineByName("eName");
     engine.eval()
