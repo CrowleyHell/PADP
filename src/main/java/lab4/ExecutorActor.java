73de76b595e49bad140b0a39a593b3e986c583fd
CrowleyHell <n.evvio@bk.ru>
2020-12-12 14:44:26 +0300

    Scripted auto-commit on change (2020-12-12 14:44:26) by gitwatch.sh

--- src/main/java/lab4/ExecutorActor.java
+++ src/main/java/lab4/ExecutorActor.java
@@ -2,8 +2,9 @@ package lab4;
 
 
 import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 
 public class ExecutorActor {
     ScriptEngine engine = new;
-    
+    ScriptEngineManager.
 }
