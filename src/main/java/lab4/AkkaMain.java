a7fe38e8aa9cdf22f0227c5b8c0128e35241df75
CrowleyHell <n.evvio@bk.ru>
2020-12-12 15:44:52 +0300

    Scripted auto-commit on change (2020-12-12 15:44:52) by gitwatch.sh

--- src/main/java/lab4/AkkaMain.java
+++ src/main/java/lab4/AkkaMain.java
@@ -3,12 +3,14 @@ package lab4;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.Http;
 
 public class AkkaMain {
     public static void main() throws Exception{
         ActorSystem actorSystem =  ActorSystem.create("Akka");
         Props props1 = Props.create(Router.class);
         ActorRef actorRef = actorSystem.actorOf(props1);
-        
+
+        final Http
     }
 }
