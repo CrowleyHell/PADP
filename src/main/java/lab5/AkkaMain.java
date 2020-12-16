0fdea4c6c63e332aa4c376829207c7436565b0dc
CrowleyHell <n.evvio@bk.ru>
2020-12-16 21:44:00 +0300

    Scripted auto-commit on change (2020-12-16 21:44:00) by gitwatch.sh

--- src/main/java/lab5/AkkaMain.java
+++ src/main/java/lab5/AkkaMain.java
@@ -26,7 +26,7 @@ public class AkkaMain {
                 ConnectHttp.toHost("localhost", 8040),
                 actorMaterializer
         );
-        System.out.println();
+        System.out.println("Server online");
     }
 
     private static Flow<HttpRequest, HttpResponse, NotUsed> actorFlow(Http http, ActorMaterializer actorMaterializer, ActorSystem actorSystem){
