3b0038a01a621479f53e4338fd50ad6e4ac4d14f
CrowleyHell <n.evvio@bk.ru>
2020-12-16 21:51:23 +0300

    Scripted auto-commit on change (2020-12-16 21:51:23) by gitwatch.sh

--- src/main/java/lab5/MainActor.java
+++ src/main/java/lab5/MainActor.java
@@ -2,7 +2,9 @@ package lab5;
 
 import akka.actor.AbstractActor;
 
+import java.util.Map;
+
 public class MainActor extends AbstractActor {
-    private final Map<String>
+    private final Map<String, >
     public Receive createReceive()
 }
