b2aaace4f52c86f80a90d82a982a153c2d7d0173
CrowleyHell <n.evvio@bk.ru>
2020-12-16 16:51:38 +0300

    Scripted auto-commit on change (2020-12-16 16:51:38) by gitwatch.sh

--- src/main/java/lab4/Router.java
+++ src/main/java/lab4/Router.java
@@ -1,13 +1,12 @@
 package lab4;
 
+import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
 
-public class Router extends AllDirectives {
-    private Route RouteCreate(ActorSystem actorSystem, ActorRef actorRef){
-        return
-    }
+public class Router extends AbstractActor {
+
 
 }
