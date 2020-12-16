115299f33e97b85e7642bc3181a904a9a96ce92b
CrowleyHell <n.evvio@bk.ru>
2020-12-16 21:35:51 +0300

    Scripted auto-commit on change (2020-12-16 21:35:51) by gitwatch.sh

--- src/main/java/lab5/AkkaMain.java
+++ src/main/java/lab5/AkkaMain.java
@@ -1,10 +1,12 @@
 package lab5;
 
+import akka.actor.ActorSystem;
+
 import java.io.IOException;
 
 public class AkkaMain {
     public static void main(String[] args) throws IOException{
         System.out.println("start");
-        
+        ActorSystem actorSystem
     }
 }
