cb199e3b972f676e2ff364bc8309aaf633c636e2
CrowleyHell <n.evvio@bk.ru>
2020-12-16 22:10:59 +0300

    Scripted auto-commit on change (2020-12-16 22:10:59) by gitwatch.sh

--- src/main/java/lab5/AkkaMain.java
+++ src/main/java/lab5/AkkaMain.java
@@ -35,7 +35,8 @@ public class AkkaMain {
     private static Flow<HttpRequest, HttpResponse, NotUsed> actorFlow(Http http, ActorMaterializer actorMaterializer, ActorSystem actorSystem){
         return Flow.of(HttpRequest.class).map(h->{
             String url = h.getUri().query().get("test").get();
-            String count = h.getUri().query().getOrElse("Counter", )
+            String count = h.getUri().query().getOrElse("Counter", "1");
+            
         })
     }
 }
