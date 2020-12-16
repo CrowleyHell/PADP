54ca09019fa183f29faea2582b44d20192d811d1
CrowleyHell <n.evvio@bk.ru>
2020-12-16 16:47:22 +0300

    Scripted auto-commit on change (2020-12-16 16:47:22) by gitwatch.sh

--- src/main/java/lab4/ExecutorActor.java
+++ src/main/java/lab4/ExecutorActor.java
@@ -8,7 +8,7 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
 public class ExecutorActor extends AbstractActor {
-    private static final String NAME = "eName";
+    private static final String NAME = "nashorn";
     private static final String SUCCESS = "true";
     private static final String FAIL = "false";
     public Receive createReceive(){
@@ -16,11 +16,11 @@ public class ExecutorActor extends AbstractActor {
             ScriptEngine engine = new ScriptEngineManager().getEngineByName(NAME);
             engine.eval(m.getJsScript());
             Invocable invocable = (Invocable) engine;
-            Object result = invocable.invokeFunction(m.getFunctionName(), m.getParams().toArray().toString());
+            String result = invocable.invokeFunction(m.getFunctionName(), m.getParams().toArray()).toString();
             if (result.equals(m.getExpectedResult())) {
-                sender().tell(new TestResult(m.getPackageID(),m.getTestName(), SUCCESS), self());
+                sender().tell(new TestResult(m.getPackageID(), m.getTestName(), SUCCESS), self());
             } else {
-                sender().tell(new TestResult(m.getPackageID(),m.getTestName(), FAIL), self());
+                sender().tell(new TestResult(m.getPackageID(), m.getTestName(), FAIL), self());
             }
         }).build();
     }
