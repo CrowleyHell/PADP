ece897161ea91322040fccb8a90e447f3389fa7c
CrowleyHell <n.evvio@bk.ru>
2020-12-16 21:54:05 +0300

    Scripted auto-commit on change (2020-12-16 21:54:05) by gitwatch.sh

--- src/main/java/lab5/MainActor.java
+++ src/main/java/lab5/MainActor.java
@@ -11,7 +11,7 @@ public class MainActor extends AbstractActor {
     public Receive createReceive(){
         return ReceiveBuilder.create()
                 .match(String.class, s -> {
-                    sender().tell(store.getOrDefault(s, ));
+                    sender().tell(store.getOrDefault(s, float(-1.0)));
                 })
     }
 }
