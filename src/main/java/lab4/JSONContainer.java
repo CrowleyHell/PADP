16eb3832b31d320a5377fc8afbca6d9fc40979da
CrowleyHell <n.evvio@bk.ru>
2020-12-16 17:24:41 +0300

    Scripted auto-commit on change (2020-12-16 17:24:41) by gitwatch.sh

--- src/main/java/lab4/JSONContainer.java
+++ src/main/java/lab4/JSONContainer.java
@@ -1,5 +1,48 @@
 package lab4;
 
+import java.util.ArrayList;
+
 public class JSONContainer {
-    
+    private final String packageID, jsScript, functionName, testName, expectedResult;
+    private final ArrayList<Integer> params;
+
+    public UnitT(String packageID, String jsScript, String functionName, String testName, String expectedResulr, ArrayList<Integer> params) {
+        this.packageID = packageID;
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.testName = testName;
+        this.expectedResult = expectedResulr;
+        this.params = params;
+    }
+
+    public String getPackageID() {
+        return packageID;
+    }
+
+    public String getJsScript() {
+        return jsScript;
+    }
+
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public String getExpectedResult() {
+        return expectedResult;
+    }
+
+    public ArrayList<Integer> getParams() {
+        return params;
+    }
+
+    @Override
+    public String toString() {
+        return "UnitT{" +
+                "params=" + params +
+                '}';
+    }
 }
