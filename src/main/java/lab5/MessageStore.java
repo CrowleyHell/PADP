684ecb55365a6ea90cf5ae95614a864806745157
CrowleyHell <n.evvio@bk.ru>
2020-12-16 21:58:22 +0300

    Scripted auto-commit on change (2020-12-16 21:58:22) by gitwatch.sh

--- src/main/java/lab5/MessageStore.java
+++ src/main/java/lab5/MessageStore.java
@@ -3,5 +3,17 @@ package lab5;
 public class MessageStore {
     private final String url;
     private final Float time;
-    
+
+    public MessageStore(String url, Float time) {
+        this.url = url;
+        this.time = time;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Float getTime() {
+        return time;
+    }
 }
