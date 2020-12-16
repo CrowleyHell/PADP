23f24cb0c0bb02da239d4ba08cf9d5f5b8edb925
CrowleyHell <n.evvio@bk.ru>
2020-12-16 23:42:40 +0300

    Scripted auto-commit on change (2020-12-16 23:42:40) by gitwatch.sh

--- src/main/java/lab5/AkkaMain.java
+++ src/main/java/lab5/AkkaMain.java
@@ -48,7 +48,8 @@ public class AkkaMain {
             return new Pair<>(url, countFloat);
         })
                 .mapAsync(2, (Pair<String, Float> pair) ->
-                        Patterns.ask(actorRef, pair.first(), 40).thenCompose(Object o)->{
+                        Patterns.ask(actorRef, pair.first(), 40).thenCompose(o)->{
+                    
 
         })
     }
