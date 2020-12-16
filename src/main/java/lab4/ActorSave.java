43c4577c2075e1b31e1e61d0e03f6148dc48284c
CrowleyHell <n.evvio@bk.ru>
2020-12-16 16:32:47 +0300

    Scripted auto-commit on change (2020-12-16 16:32:47) by gitwatch.sh

--- src/main/java/lab4/ActorSave.java
+++ src/main/java/lab4/ActorSave.java
@@ -9,19 +9,19 @@ import java.util.Map;
 
 
 public class ActorSave extends AbstractActor {
-    private Map<Integer, Map<Integer, String>> store = new HashMap<>();
+    private Map<String, Map<Integer, String>> store = new HashMap<>();
 
-    private Result IDoutput(int id){
+    private Result IDoutput(String id){
         return new Result(id, store.get(id));
     }
     @Override
     public Receive createReceive(){
-        return ReceiveBuilder.create().match(TestResult.class, m -> {
-            System.out.println("Message received" + m.getName());
-            store.put(m.getID(), new HashMap<>());
-        }).match(Integer.class, id -> {
-
-            sender().tell(IDoutput(id), self());
+        return ReceiveBuilder.create()
+                .match(TestResult.class, m -> {
+                    System.out.println("Message received" + m.getName());
+                    store.put(m.getID(), new HashMap<>()); })
+                .match(String.class, id -> {
+                    sender().tell(IDoutput(id), self());
         }).build();
     }
 
