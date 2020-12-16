1c4049aca2f5d65baf5318eb465269d3062a6e75
CrowleyHell <n.evvio@bk.ru>
2020-12-16 21:55:05 +0300

    Scripted auto-commit on change (2020-12-16 21:55:05) by gitwatch.sh

--- src/main/java/lab5/MainActor.java
+++ src/main/java/lab5/MainActor.java
@@ -1,6 +1,7 @@
 package lab5;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
 import akka.japi.pf.ReceiveBuilder;
 
 import java.util.HashMap;
@@ -11,7 +12,7 @@ public class MainActor extends AbstractActor {
     public Receive createReceive(){
         return ReceiveBuilder.create()
                 .match(String.class, s -> {
-                    sender().tell(store.getOrDefault(s, (float)-1.0), );
+                    sender().tell(store.getOrDefault(s, (float)-1.0), ActorRef.noSender());
                 })
     }
 }
