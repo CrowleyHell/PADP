23d3a1a311119d2e6f055c33a24980dace2200da
CrowleyHell <n.evvio@bk.ru>
2020-12-16 17:08:26 +0300

    Scripted auto-commit on change (2020-12-16 17:08:26) by gitwatch.sh

--- src/main/java/lab4/RouterActor.java
+++ src/main/java/lab4/RouterActor.java
@@ -3,11 +3,28 @@ package lab4;
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
+import akka.routing.ActorRefRoutee;
+import akka.routing.RoundRobinRoutingLogic;
+import akka.routing.Routee;
+import akka.routing.Router;
+import java.util.ArrayList;
 
-public class Router extends AbstractActor {
-    
+public class RouterActor extends AbstractActor {
+    private ActorRef store;
+    private Router router;
+    public RouterActor(){
+        store = getContext().actorOf(Props.create(ActorSave.class));
+        ArrayList<Routee> routees = new ArrayList<>();
+        for (int i = 0; i < 3; i++){
+            ActorRef actorRef = getContext().actorOf(Props.create(ExecutorActor.class));
+            routees.add(new ActorRefRoutee(actorRef));
+        }
+        router = new Router(new RoundRobinRoutingLogic(), routees);
+    }
+
     public Receive createReceive(){
 
     }
