cbc96ef7a9b92a87ccc9828d38286e94c1551626
CrowleyHell <n.evvio@bk.ru>
2020-12-16 21:36:34 +0300

    Scripted auto-commit on change (2020-12-16 21:36:34) by gitwatch.sh

--- src/main/java/lab5/AkkaMain.java
+++ src/main/java/lab5/AkkaMain.java
@@ -9,6 +9,7 @@ public class AkkaMain {
     public static void main(String[] args) throws IOException{
         System.out.println("start");
         ActorSystem actorSystem = ActorSystem.create("routes");
-        final Http http = h
+        final Http http = Http.get(actorSystem);
+        
     }
 }
