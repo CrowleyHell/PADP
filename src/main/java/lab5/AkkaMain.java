22dc4ff3685ef1b751e2122a37e29f10e94476d9
CrowleyHell <n.evvio@bk.ru>
2020-12-16 21:37:15 +0300

    Scripted auto-commit on change (2020-12-16 21:37:15) by gitwatch.sh

--- src/main/java/lab5/AkkaMain.java
+++ src/main/java/lab5/AkkaMain.java
@@ -3,6 +3,7 @@ package lab5;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
 
@@ -12,6 +13,6 @@ public class AkkaMain {
         ActorSystem actorSystem = ActorSystem.create("routes");
         final Http http = Http.get(actorSystem);
         final ActorMaterializer actorMaterializer = ActorMaterializer.create(actorSystem);
-        
+        final Flow<>
     }
 }
