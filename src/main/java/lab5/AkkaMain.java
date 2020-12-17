7a8a7a852ae23eb0a7559d85b22bc7987227924c
CrowleyHell <n.evvio@bk.ru>
2020-12-17 15:56:23 +0300

    Scripted auto-commit on change (2020-12-17 15:56:23) by gitwatch.sh

--- src/main/java/lab5/AkkaMain.java
+++ src/main/java/lab5/AkkaMain.java
@@ -55,6 +55,7 @@ public class AkkaMain {
                     if((float) o >= 0){
                         return CompletableFuture.completedFuture(new Pair<String, Float>(pair.first(), (float)o));
                     }
+                    Flow<>
 
         })
     }
