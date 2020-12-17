ef30c040ca31a7bbf0ad1157f3fb93093940db49
CrowleyHell <n.evvio@bk.ru>
2020-12-17 16:47:02 +0300

    Scripted auto-commit on change (2020-12-17 16:47:02) by gitwatch.sh

--- src/main/java/lab5/AkkaMain.java
+++ src/main/java/lab5/AkkaMain.java
@@ -57,8 +57,8 @@ public class AkkaMain {
                     if((float) o >= 0){
                         return CompletableFuture.completedFuture(new Pair<String, Float>(pair.first(), (float)o));
                     }
-                    Flow<Pair<String, Integer>, Long, NotUsed> floatNotUsedFlow = Flow.<Pair<String, Integer>>create()
-                            .mapConcat(param -> new ArrayList<>(Collections.nCopies()))
+                    Flow<Pair<String, Float>, Float, NotUsed> floatNotUsedFlow = Flow.<Pair<String, Float>>create()
+                            .mapConcat(param -> new ArrayList<>(Collections.nCopies(param, param)))
                             .mapAsync(pair.getValue(), r -> {
 
                             })
