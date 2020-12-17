eab69571ba1316d9c980c91bb3ecab741cb8504d
CrowleyHell <n.evvio@bk.ru>
2020-12-17 17:01:11 +0300

    Scripted auto-commit on change (2020-12-17 17:01:11) by gitwatch.sh

--- src/main/java/lab5/AkkaMain.java
+++ src/main/java/lab5/AkkaMain.java
@@ -63,7 +63,7 @@ public class AkkaMain {
                             .mapAsync(pair.second(), r -> {
                                 AsyncHttpClient asyncHttpClient = asyncHttpClient();
                                 long start = System.currentTimeMillis();
-                                asyncHttpClient.executeRequest()
+                                asyncHttpClient.prepareGet()
 
 
                             })
