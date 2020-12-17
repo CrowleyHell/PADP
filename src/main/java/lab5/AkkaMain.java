e3e610124757042daca8808318f8484ede03d966
CrowleyHell <n.evvio@bk.ru>
2020-12-17 17:03:57 +0300

    Scripted auto-commit on change (2020-12-17 17:03:57) by gitwatch.sh

--- src/main/java/lab5/AkkaMain.java
+++ src/main/java/lab5/AkkaMain.java
@@ -66,7 +66,7 @@ public class AkkaMain {
                                 asyncHttpClient.prepareGet(r).execute();
                                 return CompletableFuture.completedFuture(System.currentTimeMillis() - start);
                             });
-                    return Source.single()
+                    return Source.single(pair)
 
 
                             })
