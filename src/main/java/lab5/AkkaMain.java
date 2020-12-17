6744e200a06a2365e76a5a452ed30b0cc9271524
CrowleyHell <n.evvio@bk.ru>
2020-12-17 16:36:15 +0300

    Scripted auto-commit on change (2020-12-17 16:36:15) by gitwatch.sh

--- src/main/java/lab5/AkkaMain.java
+++ src/main/java/lab5/AkkaMain.java
@@ -59,7 +59,8 @@ public class AkkaMain {
                     }
                     Flow<Pair<String, Float>, Float, NotUsed> floatNotUsedFlow = Flow.<Pair<String, Float>>create()
                             .mapConcat(param -> {
-                                new ArrayList<>(Collections.nCopies()))
+                                new ArrayList<>(Collections.nCopies())
+
 
                             })
                             .run(actorMaterializer)
