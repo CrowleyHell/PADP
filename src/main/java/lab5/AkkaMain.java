6b5bd41eaed9dfbe179fc2b9b9860053101f67dc
CrowleyHell <n.evvio@bk.ru>
2020-12-17 16:00:45 +0300

    Scripted auto-commit on change (2020-12-17 16:00:45) by gitwatch.sh

--- src/main/java/lab5/AkkaMain.java
+++ src/main/java/lab5/AkkaMain.java
@@ -19,6 +19,7 @@ import java.util.concurrent.CompletionStage;
 
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.stream.javadsl.Source$;
 import com.zaxxer.hikari.pool.HikariPool;
 
 import static java.lang.Float.parseFloat;
@@ -55,7 +56,7 @@ public class AkkaMain {
                     if((float) o >= 0){
                         return CompletableFuture.completedFuture(new Pair<String, Float>(pair.first(), (float)o));
                     }
-                    return 
+                    return Source.
         })
     })
 }
