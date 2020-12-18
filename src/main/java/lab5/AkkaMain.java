941f077761b76c39ec1f8cc567742464f3c832b1
CrowleyHell <n.evvio@bk.ru>
2020-12-18 15:05:35 +0300

    Scripted auto-commit on change (2020-12-18 15:05:35) by gitwatch.sh

--- src/main/java/lab5/AkkaMain.java
+++ src/main/java/lab5/AkkaMain.java
@@ -80,7 +80,7 @@ public class AkkaMain {
                             .thenApply(sum -> new Pair<>(pair.first(), sum/pair.second()));
                     }))
                 .map((par) -> {
-                    actorRef.tell(new MessageStore());
+                    actorRef.tell(new MessageStore(par.first(), par.second()));
 
                         }
 
